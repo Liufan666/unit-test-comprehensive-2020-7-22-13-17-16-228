696eb1c9123702cf151eb2387d1e7e1c454739ba
Liufan666 <1663531467@qq.com>
2020-07-23 22:31:19 +0800

    [Guess Number TDD] [Kevin] feature: pass should_return_0A0B_when_guess_given_answer_1234_and_input_guess_5678

--- src/main/java/example/GuessNumebr.java
+++ src/main/java/example/GuessNumebr.java
@@ -1,6 +1,7 @@
 package example;
 
 
+import java.util.*;
 
 public class GuessNumebr {
     private int[] answer;
@@ -13,12 +14,39 @@ public class GuessNumebr {
         if(isAllCorrect(inputGuess, answer)){
             return "4A0B";
         }
+        if(getAcountOfB(inputGuess)==0){
+            return "0A0B";
+        }
         return null;
     }
     public boolean isAllCorrect(int[] inputGuess,int[] answer){
-        for (int index=0;index!=(inputGuess.length<answer.length?inputGuess.length:answer.length);index++)
+        for (int index=0;index!=(inputGuess.length<answer.length?inputGuess.length:answer.length);index++){
             if (inputGuess[index]!=(answer[index])) return false;
+        }
         return true;
     }
+    public int getAcountOfB(int[] inputGuess){
+        List list = new LinkedList();
+        int accountOfB = 0;
+        for(int index=0;index<answer.length;index++){
+            list.add(answer[index]);
+        }
+        for(int index=0;index<inputGuess.length;index++){
+            if(list.contains(inputGuess[index])){
+                accountOfB++;
+            }
+        }
+        return accountOfB;
+
+    }
+
+//    public int getAcountOfA(int[] inputGuess,int[] answer){
+//        int accountOfA = 0;
+//        for (int index=0;index!=(inputGuess.length<answer.length?inputGuess.length:answer.length);index++)
+//            if (inputGuess[index]==(answer[index])){
+//                accountOfA++;
+//            }
+//        return accountOfA;
+//    }
 
 }
