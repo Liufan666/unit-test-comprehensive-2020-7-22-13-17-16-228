a7c486c3b95018a9ae3197d374a1a6f5c3656239
Liufan666 <1663531467@qq.com>
2020-07-24 00:19:58 +0800

    [Guess Number TDD] [Kevin] refactor: finished six times of process and finished this homework

--- src/main/java/example/Main.java
+++ src/main/java/example/Main.java
@@ -4,22 +4,26 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
 
-        String str = sc.nextLine().toString();
-        String arr[] = str.split(" ");
-        int a[] = new int[arr.length];
-        for(int j = 0; j < a.length; j++)
-        {
-            a[j] = Integer.parseInt(arr[j]);
+        for(int time=0;time<6;time++){
+            Scanner sc = new Scanner(System.in);
 
-        }
-        GuessNumebr guessNumebr = new GuessNumebr(new AnswerGenerator());
-        if(guessNumebr.isLegal(a)){
-            System.out.println(guessNumebr.guess(a));
-        }
-        else {
-            System.out.println("Wrong Input，Input again");
+            String str = sc.nextLine().toString();
+            String arr[] = str.split(" ");
+            int a[] = new int[arr.length];
+            for(int j = 0; j < a.length; j++)
+            {
+                a[j] = Integer.parseInt(arr[j]);
+
+            }
+            GuessNumebr guessNumebr = new GuessNumebr(new AnswerGenerator());
+            if(guessNumebr.isLegal(a)){
+                System.out.println(guessNumebr.guess(a));
+            }
+            else {
+                System.out.println("Wrong Input，Input again");
+            }
+
         }
 
 
