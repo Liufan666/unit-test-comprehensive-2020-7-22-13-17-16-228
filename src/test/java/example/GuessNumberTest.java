cd2da3a101ba0628dea375da2f712520fbbd79cf
Liufan666 <1663531467@qq.com>
2020-07-23 22:53:41 +0800

    [Guess Number TDD] [Kevin] test: @Test

--- src/test/java/example/GuessNumberTest.java
+++ src/test/java/example/GuessNumberTest.java
@@ -68,4 +68,19 @@ public class GuessNumberTest {
         //then
         assertEquals("0A4B",result);
     }
+    @Test
+    void should_return_2A2B_when_guess_given_answer_1234_and_input_guess_1243(){
+        //given
+        int[] answer = {1,2,3,4};
+
+        AnswerGenerator answerGenerator = Mockito.mock(AnswerGenerator.class);
+
+        when(answerGenerator.generate()).thenReturn(answer);
+        GuessNumebr guessNumebr = new GuessNumebr(answerGenerator);
+        int[] inputGuess = {1,2,4,3};
+        //when
+        String result = guessNumebr.guess(inputGuess);
+        //then
+        assertEquals("2A2B",result);
+    }
 }
