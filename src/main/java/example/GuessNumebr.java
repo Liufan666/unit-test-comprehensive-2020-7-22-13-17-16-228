da6139f4d9a6e5c79a9eadaa28aecf416140f1b9
Liufan666 <1663531467@qq.com>
2020-07-23 21:16:55 +0800

    [Guess Number TDD] [Kevin] feature: pass should_return_4A0B_when_guess_given_answer_1234_and_input_guess_1234

--- src/main/java/example/GuessNumebr.java
+++ src/main/java/example/GuessNumebr.java
@@ -1,7 +1,18 @@
 package example;
 
+
+
 public class GuessNumebr {
     public String guess(int[] inputGuess, int[] answer) {
+        if(isAllCorrect(inputGuess, answer)){
+            return "4A0B";
+        }
         return null;
     }
+    public boolean isAllCorrect(int[] inputGuess,int[] answer){
+        for (int index=0;index!=(inputGuess.length<answer.length?inputGuess.length:answer.length);index++)
+            if (inputGuess[index]!=(answer[index])) return false;
+        return true;
+    }
+
 }
